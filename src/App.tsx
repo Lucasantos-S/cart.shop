8f589a9aecbc0d52ea23c7fc8311715a9f2dd829
Lucasantos-S <lucassantos.dsilv@gmail.com>
2024-05-10 12:19:22 -0300

    feat: added queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 
+import { queryClient } from './lib/react-query'
 import Routers from './router'
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
     <div className="min-h-screen">
       <HelmetProvider>
         <Helmet titleTemplate="%s | cart.shop" />
-        <Routers />
+        <QueryClientProvider client={queryClient}>
+          <Routers />
+        </QueryClientProvider>
       </HelmetProvider>
     </div>
   )
